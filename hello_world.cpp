e52492e9eb1ec0f8f12608b59e999992c7351555
shchelokov12 <aesch8877@gmail.com>
2026-05-05 00:47:48 +0300

    Add comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main() {
-    string name;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Hello world from " << name << endl;
+    string name;  // переменная для имени пользователя
+    cout << "Enter your name: ";  // запрос имени
+    cin >> name;  // чтение имени
+    cout << "Hello world from " << name << endl;  // приветствие
     return 0;
 }

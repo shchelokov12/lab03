cbfb4a7eb9a516ca499c0f90fe05ab6da2e5d513
shchelokov12 <aesch8877@gmail.com>
2026-05-05 16:16:35 +0300

    Add comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <string>
+#include <iostream>   // для std::cout, std::cin
+#include <string>      // для std::string
 
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
+    std::string name;                     // переменная для имени пользователя
+    std::cout << "Enter your name: ";     // запрос имени
+    std::cin >> name;                     // чтение имени
+    std::cout << "Hello world from " << name << std::endl;  // приветствие
     return 0;
 }
